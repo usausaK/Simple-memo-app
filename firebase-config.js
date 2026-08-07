70437a0236481350020d96f058a89063cca079e5
usausaK <131752632+usausaK@users.noreply.github.com>
2026-08-07 10:40:42 +0900

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,8 @@
-
+export const firebaseConfig = {
+  apiKey: "AIzaSyDkZJ0ShRzX0UAVZBuEfi0WZ6og9PNZ_H0",
+  authDomain: "simple-memo-app-a5ab7.firebaseapp.com",
+  projectId: "simple-memo-app-a5ab7",
+  storageBucket: "simple-memo-app-a5ab7.firebasestorage.app",
+  messagingSenderId: "172655368822",
+  appId: "1:172655368822:web:1fe45fbc4748ca0f3a5d57"
+};
